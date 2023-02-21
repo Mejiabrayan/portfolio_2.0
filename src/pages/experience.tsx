46b498bd17a8c9ddf3a30c15e6cb060d8aae8e51
mejiabrayan <learnwithbrayan@gmail.com>
2023-02-20 18:21:16 -0800

    Add experience page

---
+++ src/pages/experience.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Experience from '../../components/Experience';
+import Container from '../../components/Container';
+
+const experience = () => {
+  return (
+    <Container>
+      <Experience />
+    </Container>
+  );
+};
+
+export default experience;
