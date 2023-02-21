d9ae847835b7214e2d8a1bcbe0ff0bf058eba7a0
mejiabrayan <learnwithbrayan@gmail.com>
2023-02-20 21:12:09 -0800

    updated footer with icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,54 +1,53 @@
-import Link from 'next/link'
 import { ReactNode } from 'react';
+import { AiFillGithub, AiFillLinkedin, AiOutlineMail } from 'react-icons/ai';
 
 interface Props {
     href: string;
     children: ReactNode;
 }
 
-
-const ExternalLink = ({ href, children }: Props) => (
-    <a
-        className="text-gray-500 hover:text-gray-600 transition"
-        href={href}
-        rel="noopener noreferrer"
-        target="_blank"
-    > {children} </a>
-
-)
-
+const ExternalLink = ({ href, children }: Props) => {
+    if (href.startsWith('mailto:')) {
+        return (
+            <a
+                className="text-gray-500 hover:text-gray-600 transition"
+                href={href}
+                rel="noopener noreferrer"
+                target="_blank"
+            >
+                {children}
+            </a>
+        );
+    } else {
+        return (
+            <a
+                className="text-gray-500 hover:text-gray-600 transition"
+                href={`mailto:${href}`}
+                rel="noopener noreferrer"
+                target="_blank"
+            >
+                {children}
+            </a>
+        );
+    }
+};
 
 export default function Footer() {
     return (
-        <footer className="flex flex-col justify-center items-start max-w-2xl mx-auto w-full mb-8">
+        <footer className="flex flex-col justify-center items-center max-w-2xl mx-auto w-full mt-20 mb-8">
             <hr className="w-full border-gray-200 dark:border-gray-800 mb-8" />
             {/* Add PLayer here; maybe not */}
-            <div className='w-full max-w-2xl grid grid-cols-1 gap-4 pb-16 sm:grid-cols-3'>
-                <Link
-                    href="/"
-                    className='text-gray-500 hover:text-gray-600 transition'
-                >
-                    Home
-                </Link>
-                <Link
-                    href="/contact"
-                    className='text-gray-500 hover:text-gray-600 transition'
-                >
-                    Contact
-                </Link>
-                <Link href="/projects"
-                    className='text-gray-500 hover:text-gray-600 transition'
-
-                >
-                    Projects
-                </Link>
-                <ExternalLink href="https://mejiabrayan.github.io">
-                    Github
+            <div className="w-full max-w-xl grid grid-cols-3 justify-items-center pb-16">
+                <ExternalLink href="https://github.com/Mejiabrayan">
+                    <AiFillGithub size="1.5em" className="mx-4" />
                 </ExternalLink>
-                <ExternalLink href="https://youtube.com">
-                    Youtube
+                <ExternalLink href="https://www.linkedin.com/in/brayan-mejia/">
+                    <AiFillLinkedin size="1.5em" className="mx-4" />
+                </ExternalLink>
+                <ExternalLink href="example@example.com">
+                    <AiOutlineMail size="1.5em" className="mx-4" />
                 </ExternalLink>
             </div>
         </footer>
-    )
+    );
 }
